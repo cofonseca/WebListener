d1f3d7ab692cf9c74c35ad67dc76f281fb855b26
IAN <33247544+al-ign@users.noreply.github.com>
2017-11-24 14:51:07 +0300

    More usable examples
    Added web-friendly output (ConvertTo-HTML) examples;
    Handling of errors in ScriptBlocks;
    Reload Routes without listener restart

--- Routes.ps1
+++ Routes.ps1
@@ -25,18 +25,54 @@ $Routes = @(
             Get-Process
         }
     }
+
+    # Example of a more web-friendly route with scriptblock
     @{
         'RequestType' = 'GET'
         'RequestURL' = '/getservice'
         'ScriptBlock' = {
-            Get-Service
+            Get-Service | ConvertTo-Html
         }
     }
+
+    # Get-Process piped to ConvertTo-Html will generate a table with all process data, so it is better to filter it before output
     @{
         'RequestType' = 'GET'
         'RequestURL' = '/getprocess'
         'ScriptBlock' = {
-            Get-Process
+            Get-Process | select Name, Id, CPU, WorkingSet, Path | ConvertTo-Html
+        }
+    }
+
+    # Example of try .. catch block
+    @{
+        'RequestType' = 'GET'
+        'RequestURL' = '/getfail'
+        'ScriptBlock' = {
+        try {
+            Get-ChildItem c:\..\.. | ConvertTo-Html
+            
+            }
+        catch {
+            $Error[0] | ConvertTo-Html -as List
+            }
+
+        }
+    }
+
+    # ... Why not?
+    @{
+        'RequestType' = 'GET'
+        'RequestURL' = '/reloadroutes'
+        'ScriptBlock' = {
+        try {
+            . $Root\Routes.ps1
+            ConvertTo-Html -Body 'Reload is complete.'
+            }
+        catch {
+            $Error[0] | ConvertTo-Html -as List
+            }
+
         }
     }
 )
